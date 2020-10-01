d7716784a93724798ee31e98a6a081231fd2e91a
Maximilian Hraschan <it201503@fhstp.ac.at>
2020-10-01 12:27:13 +0200

    Added Question Interface

---
+++ src/app/services/Question.ts
@@ -0,0 +1,15 @@
+interface Question {
+
+    //Variablen
+
+    id: String;                 //FragenId
+    title: String;              //Frage
+    a1: String;                 //Antwort 1
+    a2: String;                 //Antwort 2
+    a3: String;                 //Antwort 3
+    a4: String;                 //Antwort 4
+    correct: String;            //Korrenkte Antwort
+
+    
+
+}
